e4003c441f36782264953a85d270ac1245b91967
Oliver Lorenz <mail@oliverlorenz.com>
2020-11-10 20:54:21 +0100

    fix: optimizes return

--- src/handler.ts
+++ src/handler.ts
@@ -1,18 +1,19 @@
 import { IExpressEverywhereHandler } from './handler/interface';
 import { ExpressEverywhereLocalHandler } from './handler/local';
+import { Express } from 'express';
 
 export class ExpressEverywhere {
   constructor(
     private handlerList: IExpressEverywhereHandler[] = [new ExpressEverywhereLocalHandler()],
   ) {}
 
-  getApp(type?: string) {
+  getApp(type?: string): Express {
     const handler = this.handlerList.find(handler => {
       return handler.suits(type);
     });
     if (!handler) {
       throw new Error('no handler found');
     }
-    return handler;
+    return handler.getApp();
   }
 }
